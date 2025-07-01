ed7bd36f22dbd2bfa2ddced2c878bd29346a4162
Gabriel Fogaça <gabrielfogassa@gmail.com>
2025-07-01 00:17:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function sendData(url, fetchOptions) {
+    fetch(url, fetchOptions)
+        .then(response => {
+            if (!response.ok) {
+                console.log(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
