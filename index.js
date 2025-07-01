74707cc09ccf7199cf704b6d5bb885a32e8ba4dc
Gabriel Fogaça <gabrielfogassa@gmail.com>
2025-07-01 01:30:34 -0300

    remove o arquivo de teste

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function sendData(url, fetchOptions) {
-    console.log(fetchOptions);
-    
+    var auxBody = JSON.stringify(fetchOptions.body);
+    fetchOptions.body = auxBody;
+
     fetch(url, fetchOptions)
         .then(response => {
             if (!response.ok) {
-                console.log(response.statusText);
+                console.error('API Error:', response.status, response.statusText);
+            
+                return response.text().then(text => { console.log(text || response.statusText); });
             }
-            return response.json();
+
+            // Verifica se a resposta tem um Content-Type de JSON
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+                return response.json(); // Se for JSON, parseia como JSON
+            } else {
+                // Se não for JSON, lê como texto ou assume que é vazia
+                console.warn('Response is not JSON, reading as text or assuming empty.');
+                return response.text().then(text => text ? JSON.parse(text) : {}); // Tenta parsear como JSON se tiver texto, senão retorna um objeto vazio
+            }
         })
-        .then(data => {
-            console.log('Data received:', data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
 }
